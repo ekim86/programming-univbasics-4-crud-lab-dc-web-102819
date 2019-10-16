833985b6dc98ac5eca663f04467d88e8d3288bb3
eunice kim <oonis86@gmail.com>
2019-10-16 13:47:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,10 +19,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  fav_colors =  ["wow", "red", "pink", "arrays!"]
-  wow = fav_colors.shift
-  p fav_colors
-  p wow
+  arraty = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
