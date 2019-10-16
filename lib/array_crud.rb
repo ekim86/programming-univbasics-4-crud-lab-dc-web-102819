07168cd21c5f5b37e321cf01403b82aa367e17fb
eunice kim <oonis86@gmail.com>
2019-10-16 13:39:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -48,7 +48,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-    fav_colors =  ["wow", "am", "pink", "arrays!"]
-    fav_colors[0] = "totally"
-    => "totally"
+  fav_colors =  ["wow", "am", "pink", "arrays!"]
+  fav_colors[1]
 end
