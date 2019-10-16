56bd3fc1d0dff6d61daf19c88d06c1254fca534c
eunice kim <oonis86@gmail.com>
2019-10-16 13:36:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,9 +38,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-    fav_colors =  ["blue", "am", "pink", "arrays!"]
-    fav_colors[0] = "wow"
-    => "wow"
+    fav_colors =  ["wow", "am", "pink", "arrays!"]
+    fav_colors[0]
 end
 
 def retrieve_last_element_from_array(array)
