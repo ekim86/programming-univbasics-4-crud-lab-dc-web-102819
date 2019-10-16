1351cac60f4d16108072193db976ac4cd8a0e418
eunice kim <oonis86@gmail.com>
2019-10-16 13:10:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  favorite_foods  =  ["fries", "hot dog", "hamburger", "salad"]
+  favorite_foods  <<  "fish"
+  p favorite_foods
 end
 
 def add_element_to_start_of_array(array, element)
