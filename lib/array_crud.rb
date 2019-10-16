785cf5e3c4cabe4da5b3e8785010e708f29fe22f
eunice kim <oonis86@gmail.com>
2019-10-16 13:32:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,7 +38,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+    fav_colors =  ["wow", "am", "pink", "arrays!"]
 end
 
 def retrieve_last_element_from_array(array)
